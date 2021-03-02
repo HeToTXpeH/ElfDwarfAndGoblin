b987a42b97dced1493ee46da5aefd98d30705b2a
HeToTXpeH <36816595+HeToTXpeH@users.noreply.github.com>
2021-03-02 19:22:38 +0300

    ElfException

---
+++ src/main/ElfException.java
@@ -0,0 +1,8 @@
+package main;
+
+public class ElfException extends ExceptionInInitializerError{
+
+    public ElfException(String s) {
+        super(s);
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -9,27 +9,34 @@ public class Main {
         arr[1] = new Elf();
         arr[2] = new Goblin();
 
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 3; i++){
-            System.out.println("Question to the first person: Who is on the (write right or left)");
-            String text = scanner.nextLine();
-            if (text.equals("right"))
-                System.out.println("At right is " + arr[i].getPerson(arr[(i + 1)%3]));
-            if (text.equals("left"))
-                System.out.println("At left is " + arr[i].getPerson(arr[(i + 2)%3]));
-        }
-        boolean a = true;
-        for (int i = 0; i < 3; i++){
-            System.out.println("Who is? If its Elf print E if Goblin print G if Dwarf print D");
-            String text = scanner.nextLine();
-            if (!(text.equals(arr[i].WhoIs()))){
-                a = false;
-                break;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Question to the first person: Who is on the (write right or left)");
+                String text = scanner.nextLine();
+                if (text.equals("right"))
+                    System.out.println("At right is " + arr[i].getPerson(arr[(i + 1) % 3]));
+                else if (text.equals("left"))
+                    System.out.println("At left is " + arr[i].getPerson(arr[(i + 2) % 3]));
+                else
+                    throw new ElfException("Input error");
             }
+            boolean a = true;
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Who is? If its Elf print E if Goblin print G if Dwarf print D");
+                String text = scanner.nextLine();
+                if (!(text.equals(arr[i].WhoIs()))) {
+                    a = false;
+                    break;
+                }
+            }
+            if (a)
+                System.out.println("U win!");
+            else
+                System.out.println("U lost");
         }
-        if (a)
-            System.out.println("U win!");
-        else
-            System.out.println("U lost");
+        catch (ElfException a) {
+            System.out.println(a.toString());
+        }
     }
 }
